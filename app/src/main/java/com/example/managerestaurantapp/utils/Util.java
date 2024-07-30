b962333c5b90e8645c8616dbfa694c831fb07ce6
DoanQuangPhuong <quangphuong080603@gmail.com>
2024-07-31 00:07:44 +0700

    Upload file

---
+++ app/src/main/java/com/example/managerestaurantapp/utils/Util.java
@@ -0,0 +1,9 @@
+package com.example.managerestaurantapp.utils;
+
+import com.example.managerestaurantapp.models.User;
+
+public class Util {
+    public static final String BASE_URL2 = "http://192.168.114.1:2109/dangnhap/";
+    public static final String BASE_URL = "http://192.168.114.1/apirestaurant/";
+    public static User user_current = new User();
+}
